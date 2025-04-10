d7e4039ba199e332176e4a0814ce27af3cb8572d
blessed <ytminicraftyt@gmail.com>
2025-04-10 20:32:44 +0300

    upd2

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
+from typing import List, Optional
+
 from fastapi import FastAPI, HTTPException, status, Query, Path
 from fastapi.responses import JSONResponse
 import uvicorn
 
 from data_actions import get_db, save_db
-from models import SpellModel
+from models import SpellModel, SpellModelResponse, UserModel
 
 
 app = FastAPI()
 
 
-@app.get("/spells/")
+@app.get("/spells/", response_model=List[SpellModelResponse], status_code=status.HTTP_202_ACCEPTED)
 async def get_spells():
     return get_db()
 
 
-@app.get("/spells/{index}/")
+@app.get("/spells/{index}/", response_model=SpellModelResponse, status_code=status.HTTP_202_ACCEPTED)
 async def get_spell(index: int):
     spell = next((spell for spell in get_db() if spell.get("index") == index), None)
+    if not spell:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Заклинання з id {index} не знайдено. ☹️")
     return spell
-
-
-# @app.post("/spells/")
-# async def add_spell(
-#     index: int = Query(..., description="Номер по порядку", gt=71),
-#     spell: str = Query(..., description="Нове заклинання"),
-#     use: str = Query(..., description="Опис")
-# ):
-#     new_spell = dict(index=index, spell=spell, use=use)
-#     db = get_db()
-#     db.append(new_spell)
-#     save_db(db)
-#     return "Нове заклинання успішно додано!"
 
 
 @app.post("/spells/", status_code=status.HTTP_201_CREATED)
@@ -38,7 +29,14 @@ async def add_spell(spell_model: SpellModel):
     db = get_db()
     db.append(spell_model.model_dump())
     save_db(db)
-    return JSONResponse("Нове заклинання успішно додано!")
+    return JSONResponse("Нове заклинання успішно додано! 😎")
+
+
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+def add_user(user: UserModel):
+    return JSONResponse("Користувача успішно додано! 😎")
+
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,9 +1,42 @@
 from typing import Union, Optional
 
-from pydantic import BaseModel, field_validator, Field
+from pydantic import BaseModel, field_validator, Field, EmailStr
 
+class MyException(BaseException):
+    pass
 
 class SpellModel(BaseModel):
+    index: int = Field(..., description="Індекс запису")#, gt=0, lt=100)
+    spell: str = Field(..., description="Нове заклинання")
+    use: Optional[str] = Field(None, description="Використання")
+
+
+    @field_validator("index")
+    def check_index(cls, index):
+        if 0 < index < 100:
+            return index
+        raise ValueError("Індекс повинен бути у діапазоні від 0 до 100 ☹️")
+
+    @field_validator("spell")
+    def check_spell(cls, spell):
+        if len(spell) > 3:
+            return spell.upper()
+        raise ValueError("Заклинання повинно бути довшим за 3 символи ☹️")
+
+class SpellModelResponse(BaseModel):
     index: int = Field(..., description="Індекс запису")
     spell: str = Field(..., description="Нове заклинання")
     use: Optional[str] = Field(None, description="Використання")
+
+
+class UserModel(BaseModel):
+    first_name: str = Field(...)
+    email: EmailStr = Field(...)
+
+    @field_validator("first_name")
+    def check_first_name(cls, first_name: str):
+        if all([first_name.isalpha() and len(first_name) >= 2]):
+            return first_name
+        raise ValueError("Ім'я повинно мати мінімум 2 символи, лише літери ☹️")
+
+    
